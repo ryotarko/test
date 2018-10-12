a923bee9508f2c425643f7446556dbf50f5d93e2
ryotarko <gottsu02ss11@gmail.com>
2018-10-12 10:13:08 +0900

    2018/10/12

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b4/20b9c4d33fc800181f8ef262df9b79d8
@@ -0,0 +1,6 @@
+
+public class Test {
+	public String name = null;
+	public int age = 0;
+
+}
---
+++ Hello World/src/hello.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ */
+
+/**
+ * @author internousdev
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = "123";
+		int i = Integer.parseInt(s);
+        System.out.println(s);
+
+	}
+}
